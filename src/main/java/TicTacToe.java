a22fef46c97d5fd299fe723983423a7b912a0abe
Orri Arnarsson <orriarnars@gmail.com>
2016-10-26 11:53:54 +0000

    Restructure
    New .java files created.
    build.gradle updated
    Folder structure updated.

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,17 @@
+package ttt;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		String board =  new String();
+		board = "123456789";
+
+		PrintBoard(board);
+
+		System.out.println("Synergy welcomes you to TicTacToe!\n");
+	}
+
+	public static String TestingTests() {
+		return ("Tests are connected and running");
+	}
+}
---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,13 @@
+package ttt;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+
+  @Test //0
+  public void testConnection() {
+      assertEquals("Tests are connected and running", TicTacToe.TestingTests());
+  }
+}
